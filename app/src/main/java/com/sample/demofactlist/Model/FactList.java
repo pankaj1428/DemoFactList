ab60c3ff3f04c9a05ba21ab9f69424145d09b12b
pankaj <pankajmishra1428@gmail.com>
2019-01-03 01:34:45 +0530

    implemented Model classes for server response

---
+++ app/src/main/java/com/sample/demofactlist/Model/FactList.java
@@ -0,0 +1,33 @@
+package com.sample.demofactlist.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+public class FactList {
+
+    @SerializedName("title")
+    @Expose
+    private String title;
+
+    @SerializedName("rows")
+    @Expose
+    private ArrayList<Row> rows = null;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public ArrayList<Row> getRows() {
+        return rows;
+    }
+
+    public void setRows(ArrayList<Row> rows) {
+        this.rows = rows;
+    }
+}
